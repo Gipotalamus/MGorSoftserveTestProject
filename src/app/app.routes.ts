ad11da95faf49340c8999291bfd08f3d79ddf695
Gipotalamus <hectop23@ukr.net>
2017-04-23 23:28:23 +0300

    responsive layout first try

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import {Route} from '@angular/router';
 import {GeneralComponent} from "./general/general.component";
 
 export const routes: Route[] = [
-  {path: ':direction', component: GeneralComponent}
-  //{path: '', redirectTo: 'general', pathMatch: 'full'}
+  {path: ':direction', component: GeneralComponent},
+  {path: '', redirectTo: 'general', pathMatch: 'full'}
   ];
